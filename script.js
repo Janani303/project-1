3f41e7290731c2333a78876896d6c9497293286d
Janani303 <84324472+Janani303@users.noreply.github.com>
2021-05-24 13:35:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-let body = document.getElementById("body");
-console.log(body)
-
-let profileImg = document.getElementById("profile-img");
-
-let table = document.getElementById("table");
-console.log(table)
-
-let th = document.getElementById("th");
-console.log(th)
-
-let modeToggle = document.getElementById("mode-toggle");
-console.log(modeToggle)
+let API_KEY = "1c322ca3c0bcff4e090a754e8b1b8091";
 
 
-function changeMode() {
-    
-    if (modeToggle.innerText === "Light") {
-        modeToggle.innerText = "Dark";
-    }else {
-        modeToggle.innerText = "Light";
-    }
-
-    body.classList.toggle("dark-body");
-    body.classList.toggle("light-body");
-    modeToggle.classList.toggle("light-toggle");
-    modeToggle.classList.toggle("dark-toggle");
-    profileImg.classList.toggle("light-profile-img");
-    profileImg.classList.toggle("dark-profile-img");
-    table.classList.toggle("dark-table");
-    table.classList.toggle("light-table");
-    th.classList.toggle("dark-toggle");
-    th.classList.toggle("light-toggle");
+getWeatherdata = (city) => {
+ const URL = "https://api.openweathermap.org/data/2.5/weather";
+ const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+ const weatherPromise = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+   return response.json();
+ })
 }
-modeToggle.addEventListener("click", changeMode);
+
+
+
+searchCity = () => {
+ const city = document.getElementById('city-input').value;
+ getWeatherdata(city)
+ .then((response) => {
+  showWeatherData(response);
+ }).catch((error) => {
+  console.log(error);
+  } )
+ }
+
+
+ showWeatherData = (weatherData) => {
+  document.getElementById('city-name').innerText = weatherData.name;
+  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
+  document.getElementById('temp').innerText = weatherData.main.temp;
+  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
+  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
+
+ }
