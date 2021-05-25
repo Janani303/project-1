f8da8c639553b0f0e7a3b3101ca16447760a38b0
Janani303 <84324472+Janani303@users.noreply.github.com>
2021-05-25 19:04:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
-let API_KEY = "1c322ca3c0bcff4e090a754e8b1b8091";
 
+/* Number Guessing Game JavaScript */
 
-getWeatherdata = (city) => {
- const URL = "https://api.openweathermap.org/data/2.5/weather";
- const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
- const weatherPromise = fetch(FULL_URL);
-  return weatherPromise.then((response) => {
-   return response.json();
- })
+var secretNumber = 0,
+	numberOfGuesses = 0;
+
+function writeMessage(elementId, message, appendMessage) {
+	var elemToUpdate = document.getElementById(elementId);
+	if (appendMessage) {
+		elemToUpdate.innerHTML = elemToUpdate.innerHTML + message;
+	} else {
+		elemToUpdate.innerHTML = message;
+	}
+};
+
+function newGame() {
+	secretNumber = Math.floor(Math.random() * 100) + 1;
+	numberOfGuesses = 0;
+	writeMessage('historyList', '');
 }
 
+function guessInRange(guess) {
+	return (guess > 0 && guess < 101);
+}
 
+function userGuessed() {
+	var userGuessed = document.getElementById('userGuess').value;
+	var statusArea = document.getElementById('statusArea');
+	var historyList = document.getElementById('historyList');
+	if (userGuessed.length == 0 || ! guessInRange(userGuessed)) {
+		// Nothing entered or our of range.
+		writeMessage('statusArea', '<p>Please enter a number 1-100 and press the Guess button.</p>');
+	} else if (userGuessed.indexOf('.') != -1) {
+		writeMessage('statusArea', '<p>Please enter a whole number 1-100 and press the Guess button.</p>');
+	} else {
+		numberOfGuesses++;
 
-searchCity = () => {
- const city = document.getElementById('city-input').value;
- getWeatherdata(city)
- .then((response) => {
-  showWeatherData(response);
- }).catch((error) => {
-  console.log(error);
-  } )
- }
+		if (userGuessed == secretNumber) {
+			// Got it
+			writeMessage('statusArea', '<p>You got me in ' + numberOfGuesses +' guesses, I was thinking ' + secretNumber + '. Let\'s go again...</p>');
+			newGame();
+		} else if (userGuessed < secretNumber) {
+			// User needs to guess higher
+			writeMessage('statusArea', '<p>You need to guess higher than ' + userGuessed + ', try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed +' (too low)</li>', true);
+		} else {
+			// User needs to guess lower
+			writeMessage('statusArea', '<p>You need to guess lower than ' + userGuessed + ', try again...</p>');
+			writeMessage('historyList', '<li>' + userGuessed + ' (too high)</li>', true);
+		}
+	}
 
+	document.getElementById('userGuess').value = '';	
+}
 
- showWeatherData = (weatherData) => {
-  document.getElementById('city-name').innerText = weatherData.name;
-  document.getElementById('weather-type').innerText = weatherData.weather[0].main;
-  document.getElementById('temp').innerText = weatherData.main.temp;
-  document.getElementById('min-temp').innerText = weatherData.main.temp_min;
-  document.getElementById('max-temp').innerText = weatherData.main.temp_max;
-
- }
+window.onload = function() {
+	newGame();
+	document.getElementById('buttonArea').addEventListener('click', userGuessed);
+};
